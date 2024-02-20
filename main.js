435f05ec53fd70e99dce0f74c63facce09d5ee6d
Jamsdfn <2419252842@qq.com>
2024-02-20 14:12:06 +0800

    init

---
+++ main.js
@@ -0,0 +1,38 @@
+const { BrowserWindow, ipcMain } = require('electron')
+const { app } = require('electron')
+const path = require('path')
+
+const isDevelopment = process.env.NODE_ENV === 'development'
+let mainWindow = null
+
+function createMainWindow() {
+    mainWindow = new BrowserWindow({
+        width: 1160,
+        height: 752,
+        minHeight: 632,
+        minWidth: 960,
+        show: false,
+        // frame: false,
+        title: 'Editor',
+        // webPreferences: {
+        //     nodeIntegration: true
+        //     // preload: path.resolve(__dirname, '../utils/contextBridge.js')
+        // },
+        icon: path.resolve(__dirname, './public/logo192.png')
+    })
+
+    if (isDevelopment) {
+        mainWindow.loadURL('http://localhost:3000/')
+    } else {
+        const entryPath = path.resolve(__dirname, '../../build/index.html')
+        mainWindow.loadFile(entryPath)
+    }
+
+    mainWindow.once('ready-to-show', () => {
+        mainWindow.show()
+    })
+}
+
+app.on('ready', () => {
+    createMainWindow()
+})
